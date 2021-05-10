9255b8f46790ac78eb152f39f21e712493326918
tomon9086 <tomon9086@gmail.com>
2021-05-11 01:50:30 +0900

    dekita!!!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use piston_window::*;
 use rand::prelude::*;
-use std::{thread, time};
 
 struct Map {
   width: usize,
@@ -12,7 +11,6 @@ struct Map {
 struct Cell {
   x: usize,
   y: usize,
-  // color: types::Color,
   is_alive: bool,
 }
 
@@ -137,83 +135,64 @@ impl Map {
 }
 
 fn open_window(width: u32, height: u32, title: &'static str) -> PistonWindow {
-  return WindowSettings::new(title, [width, height])
+  let mut window: PistonWindow = WindowSettings::new(title, [width, height])
     .exit_on_esc(true)
     .build()
     .unwrap();
+  // window.set_lazy(true);
+  window.events.set_ups(2);
+  return window;
 }
 
-fn draw(window: &mut PistonWindow, map: &Map) {
+fn convert_coord_to_rect(
+  map: &Map,
+  window_size: Size,
+  x: usize,
+  y: usize,
+) -> (types::Color, types::Rectangle) {
   let width = map.width;
   let height = map.height;
   let dead_color = [0., 0., 0., 1.];
   let alive_color = [0., 1., 0., 1.];
   let frame_width = 4.0;
   let border_width = 2.0;
-  let (window_width, window_height) = (window.size().width, window.size().height);
+  let (window_width, window_height) = (window_size.width, window_size.height);
   let (cell_width, cell_height) = (
     (window_width - 2. * frame_width + border_width) / (width as f64) - border_width,
     (window_height - 2. * frame_width + border_width) / (height as f64) - border_width,
   );
-  if let Some(event) = window.next() {
-    println!("drawing!!!!");
-    window.draw_2d(&event, |context, graphics, _device| {
-      clear([0.; 4], graphics);
-      for x in 0..width {
-        for y in 0..height {
-          println!("{} {}", x, y);
-          let cell = map.cells[width * y + x];
-          let px = frame_width + (x as f64) * (border_width + cell_width);
-          let py = frame_width + (y as f64) * (border_width + cell_height);
-          let mut color = dead_color;
-          if cell.is_alive {
-            color = alive_color;
-          }
-          rectangle(
-            color,
-            [px, py, cell_width, cell_height],
-            context.transform,
-            graphics,
-          );
-        }
-      }
-    });
+  let cell = map.cells[width * y + x];
+  let px = frame_width + (x as f64) * (border_width + cell_width);
+  let py = frame_width + (y as f64) * (border_width + cell_height);
+  let mut color = dead_color;
+  if cell.is_alive {
+    color = alive_color;
   }
+  return (color, [px, py, cell_width, cell_height]);
 }
 
 fn main() {
-  // let mut window = open_window(240, 240, "Game of Life in Rust");
+  let mut window = open_window(240, 240, "Game of Life in Rust");
 
   let mut map = Map::new(10, 10);
   map.randomize();
-  for g in 0..30 {
-    // draw(&mut window, &map);
 
-    let cells = map.get_2d_cells();
-    println!(
-      "{}\n",
-      cells
-        .into_iter()
-        .map(|row| {
-          return row
-            .into_iter()
-            .map(|cell| {
-              if cell.is_alive {
-                return "@".to_string();
-              } else {
-                return "-".to_string();
-              }
-            })
-            .collect::<Vec<String>>()
-            .join(" ");
-        })
-        .collect::<Vec<String>>()
-        .join("\n")
-    );
-
-    map.next_generation();
-
-    thread::sleep(time::Duration::from_millis(500));
+  let window_size = window.size();
+  while let Some(event) = window.next() {
+    window.draw_2d(&event, |context, graphics, _device| {
+      clear([0.; 4], graphics);
+      for x in 0..map.width {
+        for y in 0..map.height {
+          let rect = convert_coord_to_rect(&map, window_size, x, y);
+          rectangle(rect.0, rect.1, context.transform, graphics);
+        }
+      }
+    });
+    if let Some(_) = event.update_args() {
+      map.next_generation();
+    }
+    if let Some(_) = event.press_args() {
+      map.next_generation();
+    }
   }
-  // thread::sleep(time::Duration::from_millis(10000));
 }
